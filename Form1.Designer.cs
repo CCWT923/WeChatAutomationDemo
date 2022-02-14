024edd5c02dda17f923759e1846121224d9701f5
WuTao <ccwutao@gmail.com>
2022-02-14 18:13:06 +0800

    增加定时发送消息的功能。

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,59 +29,179 @@ namespace WeChatAutomationDemo
         /// </summary>
         private void InitializeComponent()
         {
-            this.Btn_ActiveWeChat = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.Btn_WeChatButtonTest = new System.Windows.Forms.Button();
+            this.components = new System.ComponentModel.Container();
+            this.TextBox_Log = new System.Windows.Forms.TextBox();
+            this.TextBox_AppPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Btn_FindFile = new System.Windows.Forms.Button();
+            this.TextBox_SendTarget = new System.Windows.Forms.TextBox();
+            this.TextBox_Message = new System.Windows.Forms.TextBox();
+            this.Button_SendMessage = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.CheckBox_ScheduleTask = new System.Windows.Forms.CheckBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // Btn_ActiveWeChat
+            // TextBox_Log
             // 
-            this.Btn_ActiveWeChat.Location = new System.Drawing.Point(12, 12);
-            this.Btn_ActiveWeChat.Name = "Btn_ActiveWeChat";
-            this.Btn_ActiveWeChat.Size = new System.Drawing.Size(137, 56);
-            this.Btn_ActiveWeChat.TabIndex = 1;
-            this.Btn_ActiveWeChat.Text = "发送微信消息";
-            this.Btn_ActiveWeChat.UseVisualStyleBackColor = true;
-            this.Btn_ActiveWeChat.Click += new System.EventHandler(this.button1_Click);
+            this.TextBox_Log.Location = new System.Drawing.Point(12, 258);
+            this.TextBox_Log.Margin = new System.Windows.Forms.Padding(2);
+            this.TextBox_Log.Multiline = true;
+            this.TextBox_Log.Name = "TextBox_Log";
+            this.TextBox_Log.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.TextBox_Log.Size = new System.Drawing.Size(571, 94);
+            this.TextBox_Log.TabIndex = 2;
             // 
-            // textBox1
+            // TextBox_AppPath
             // 
-            this.textBox1.Location = new System.Drawing.Point(197, 12);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox1.Size = new System.Drawing.Size(591, 426);
-            this.textBox1.TabIndex = 2;
+            this.TextBox_AppPath.Location = new System.Drawing.Point(82, 12);
+            this.TextBox_AppPath.Name = "TextBox_AppPath";
+            this.TextBox_AppPath.Size = new System.Drawing.Size(384, 21);
+            this.TextBox_AppPath.TabIndex = 3;
             // 
-            // Btn_WeChatButtonTest
+            // label1
             // 
-            this.Btn_WeChatButtonTest.Location = new System.Drawing.Point(12, 103);
-            this.Btn_WeChatButtonTest.Name = "Btn_WeChatButtonTest";
-            this.Btn_WeChatButtonTest.Size = new System.Drawing.Size(137, 56);
-            this.Btn_WeChatButtonTest.TabIndex = 1;
-            this.Btn_WeChatButtonTest.Text = "测试微信按钮";
-            this.Btn_WeChatButtonTest.UseVisualStyleBackColor = true;
-            this.Btn_WeChatButtonTest.Click += new System.EventHandler(this.Btn_WeChatButtonTest_Click);
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "微信位置：";
+            // 
+            // Btn_FindFile
+            // 
+            this.Btn_FindFile.Location = new System.Drawing.Point(486, 10);
+            this.Btn_FindFile.Name = "Btn_FindFile";
+            this.Btn_FindFile.Size = new System.Drawing.Size(75, 23);
+            this.Btn_FindFile.TabIndex = 5;
+            this.Btn_FindFile.Text = "浏览";
+            this.Btn_FindFile.UseVisualStyleBackColor = true;
+            this.Btn_FindFile.Click += new System.EventHandler(this.Btn_FindFile_Click);
+            // 
+            // TextBox_SendTarget
+            // 
+            this.TextBox_SendTarget.Location = new System.Drawing.Point(79, 31);
+            this.TextBox_SendTarget.Name = "TextBox_SendTarget";
+            this.TextBox_SendTarget.Size = new System.Drawing.Size(383, 21);
+            this.TextBox_SendTarget.TabIndex = 6;
+            this.TextBox_SendTarget.Text = "文件传输助手";
+            // 
+            // TextBox_Message
+            // 
+            this.TextBox_Message.Location = new System.Drawing.Point(79, 68);
+            this.TextBox_Message.Multiline = true;
+            this.TextBox_Message.Name = "TextBox_Message";
+            this.TextBox_Message.Size = new System.Drawing.Size(384, 98);
+            this.TextBox_Message.TabIndex = 6;
+            this.TextBox_Message.Text = "测试内容";
+            // 
+            // Button_SendMessage
+            // 
+            this.Button_SendMessage.Location = new System.Drawing.Point(474, 24);
+            this.Button_SendMessage.Margin = new System.Windows.Forms.Padding(2);
+            this.Button_SendMessage.Name = "Button_SendMessage";
+            this.Button_SendMessage.Size = new System.Drawing.Size(97, 32);
+            this.Button_SendMessage.TabIndex = 1;
+            this.Button_SendMessage.Text = "发送消息";
+            this.Button_SendMessage.UseVisualStyleBackColor = true;
+            this.Button_SendMessage.Click += new System.EventHandler(this.Button_SendMessage_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(8, 34);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 12);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "发送目标：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "消息内容：";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dateTimePicker1);
+            this.groupBox1.Controls.Add(this.CheckBox_ScheduleTask);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.TextBox_Message);
+            this.groupBox1.Controls.Add(this.Button_SendMessage);
+            this.groupBox1.Controls.Add(this.TextBox_SendTarget);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Location = new System.Drawing.Point(12, 39);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(576, 214);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(157, 181);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(172, 21);
+            this.dateTimePicker1.TabIndex = 8;
+            // 
+            // CheckBox_ScheduleTask
+            // 
+            this.CheckBox_ScheduleTask.AutoSize = true;
+            this.CheckBox_ScheduleTask.Location = new System.Drawing.Point(79, 181);
+            this.CheckBox_ScheduleTask.Name = "CheckBox_ScheduleTask";
+            this.CheckBox_ScheduleTask.Size = new System.Drawing.Size(72, 16);
+            this.CheckBox_ScheduleTask.TabIndex = 7;
+            this.CheckBox_ScheduleTask.Text = "定时任务";
+            this.CheckBox_ScheduleTask.UseVisualStyleBackColor = true;
+            this.CheckBox_ScheduleTask.CheckedChanged += new System.EventHandler(this.CheckBox_ScheduleTask_CheckedChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.Btn_WeChatButtonTest);
-            this.Controls.Add(this.Btn_ActiveWeChat);
+            this.ClientSize = new System.Drawing.Size(600, 360);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.Btn_FindFile);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TextBox_AppPath);
+            this.Controls.Add(this.TextBox_Log);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.Button Btn_ActiveWeChat;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button Btn_WeChatButtonTest;
+        private System.Windows.Forms.TextBox TextBox_Log;
+        private System.Windows.Forms.TextBox TextBox_AppPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Btn_FindFile;
+        private System.Windows.Forms.TextBox TextBox_SendTarget;
+        private System.Windows.Forms.TextBox TextBox_Message;
+        private System.Windows.Forms.Button Button_SendMessage;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.CheckBox CheckBox_ScheduleTask;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -11,6 +11,7 @@ using System.Windows.Forms;
 using Application = FlaUI.Core.Application;
 using FlaUI.Core.Definitions;
 using FlaUI.Core.WindowsAPI;
+using System.IO;
 
 namespace WeChatAutomationDemo
 {
@@ -21,96 +22,123 @@ namespace WeChatAutomationDemo
             InitializeComponent();
         }
 
-        Application WeChatApp = null;
-        Window Wnd_WeChat = null;
-        AutomationElement Desktop;
-        private void button1_Click(object sender, EventArgs e)
+        
+
+        const string appDefaultPath = @"C:\Program Files (x86)\Tencent\WeChat\WeChat.exe";
+
+        /// <summary>
+        /// 获取微信的主程序对象
+        /// </summary>
+        /// <returns></returns>
+        private Application GetWeChatApplication()
         {
-            if(WeChatApp == null)
+            //程序是否在运行
+            var targetId = GetProcessId("WeChat");
+            if (targetId == 0)
             {
-                WriteLog("正在尝试连接到微信主程序。");
-                var targetId = GetProcessId("WeChat");
-                if(targetId == 0)
+                if (TextBox_AppPath.Text != "" && File.Exists(TextBox_AppPath.Text))
                 {
-                    WriteLog("没有找到微信的进程，请确定微信是否启动！");
-                    return;
+                    WriteLog("启动微信。");
+                    return Application.Launch(TextBox_AppPath.Text);
                 }
-                WeChatApp = Application.Attach(targetId);
-                WriteLog("连接微信成功。");
-                /**
-                 * 微信，如果隐藏在托盘区域的话（非最小化状态），那么试图获取主窗口会一直卡住，直到手动点击微信
-                 * 图标将其窗口显示出来，程序才能正常运行。
-                 * 可以通过查找桌面的子元素，看看是否有微信的窗口来判断其是否隐藏在托盘区
-                 * 
-                 */
-                Desktop = new UIA3Automation().GetDesktop();
             }
-            WriteLog("正在激活微信。");
+            else
+            {
+               return Application.Attach(targetId);
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// 获取微信的主窗口
+        /// </summary>
+        /// <param name="app"></param>
+        /// <returns></returns>
+        private Window GetWeChatMainWindow(ref Application app)
+        {
+            AutomationElement desktop = new UIA3Automation().GetDesktop();
             ConditionFactory cf = new ConditionFactory(new UIA3PropertyLibrary());
+            /**
+            * 微信，如果隐藏在托盘区域的话（非最小化状态），那么试图获取主窗口会一直卡住，直到手动点击微信
+            * 图标将其窗口显示出来，程序才能正常运行。
+            * 可以通过查找桌面的子元素，看看是否有微信的窗口来判断其是否隐藏在托盘区
+            * 
+            */
             //查找「通知V形」按钮并单击
-            var btn_NotifyMore = Desktop.FindFirstDescendant(cf.ByAutomationId("1502")).AsButton();
+            var btn_NotifyMore = desktop.FindFirstDescendant(cf.ByAutomationId("1502")).AsButton();
             btn_NotifyMore.Click();
-            
+
             AutomationElement btn_WeChat_TrayButton = null;
             AutomationElement Panel_NotifyOverFlowArea = null;
             Retry.WhileNull(() =>
             {
-                Panel_NotifyOverFlowArea = Desktop.FindFirstDescendant(cf.ByName("通知溢出")).AsWindow();
+                Panel_NotifyOverFlowArea = desktop.FindFirstDescendant(cf.ByName("通知溢出")).AsWindow();
                 return Panel_NotifyOverFlowArea;
             }, TimeSpan.FromSeconds(5), null, true, false, "没有找到「通知溢出」的按钮。");
             //查找并点击托盘区域微信的图标
-            //微信按钮：{DDC66FF1-5768-0E74-ABB5-9025FBC4CFDA}
-            btn_WeChat_TrayButton = Panel_NotifyOverFlowArea.FindFirstDescendant(cf.ByAutomationId("{DDC66FF1-5768-0E74-ABB5-9025FBC4CFDA}")).AsButton();
+            btn_WeChat_TrayButton = Panel_NotifyOverFlowArea.FindFirstDescendant(cf.ByName("微信")).AsButton();
             btn_WeChat_TrayButton.Click();
             Thread.Sleep(300);
             btn_NotifyMore.Click();
             //微信主窗口
-            Wnd_WeChat = WeChatApp.GetMainWindow(new UIA3Automation());
-            if(GetWindowVisibleStatus("微信"))
-            {
-                WriteLog("微信窗口已激活。");
-            }
-            Wnd_WeChat.Focus();
+            return app.GetMainWindow(new UIA3Automation());
+        }
+
+        /// <summary>
+        /// 搜索指定的对象并激活其聊天窗口
+        /// </summary>
+        /// <param name="wechatWindow"></param>
+        /// <param name="target"></param>
+        /// <returns>返回指定搜索对象的聊天输入框</returns>
+        private AutomationElement SearchTarget(in Window wechatWindow, string target)
+        {
             //获取并单击「聊天」按钮，如果直接将其 AsButton() 单击的话，会出现找不到点击位置的异常
             //所以这里获取按钮的矩形，然后点击其中间
-            WriteLog("激活「聊天」界面。");
-            var Btn_Chat = GetWeChatControl("聊天", ControlType.Button);
-            if(Btn_Chat == null)
+            var Btn_Chat = GetWeChatControl(wechatWindow,"聊天", ControlType.Button);
+            if (Btn_Chat == null)
             {
                 WriteLog("没有获取到微信「聊天」按钮。");
-                return;
+                return null;
             }
             var rect = Btn_Chat.BoundingRectangle;
             Mouse.Click(new Point(rect.X + (rect.Width / 2), rect.Y + (rect.Height / 2)));
             Thread.Sleep(500);
-            string targetChatObject = "文件传输助手";
-            WriteLog("搜索指定内容：" + targetChatObject);
-            var edit_SearchChatObject = GetWeChatControl("搜索", ControlType.Edit);
-            if(edit_SearchChatObject == null)
+
+            var edit_SearchChatObject = GetWeChatControl(in wechatWindow,"搜索", ControlType.Edit);
+            if (edit_SearchChatObject == null)
             {
                 WriteLog("没有找到指定的控件：搜索框。");
-                return;
+                return null;
             }
+            WriteLog("搜索指定内容：" + target);
             rect = edit_SearchChatObject.BoundingRectangle;
             Mouse.Click(new Point(rect.X + (rect.Width / 2), rect.Y + (rect.Height / 2)));
-            edit_SearchChatObject.AsTextBox().Enter(targetChatObject);
+            edit_SearchChatObject.AsTextBox().Enter(target);
             Thread.Sleep(1000);
             Keyboard.Press(VirtualKeyShort.ENTER);
+
             //查找发送消息的文本框
-            WriteLog("查找消息发送编辑框，并输入文字。");
-            var edit_MessageInput = GetWeChatControl("输入", ControlType.Edit);
-            if(edit_MessageInput == null)
+            var edit_MessageInput = GetWeChatControl(in wechatWindow, "输入", ControlType.Edit);
+            if (edit_MessageInput == null)
             {
                 WriteLog("没有找到消息输入的控件。");
-                return;
+                return null;
             }
-            string messageToSend = "";//string.Format("{1}    {2}",DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss.FFF"),"UI Automation for WeChat(By:WuTao).");
-            rect = edit_MessageInput.BoundingRectangle;
+            return edit_MessageInput;
+        }
+
+        private void SendMessage(in Window wechatWindow, in AutomationElement chatInputBux, string message)
+        {
+            var rect = chatInputBux.BoundingRectangle;
             Mouse.Click(new Point(rect.X + (rect.Width / 2), rect.Y + (rect.Height / 2)));
             Thread.Sleep(500);
 
             //查找「发送」按钮，并单击发送消息
-            var btn_SendMessage = GetWeChatControl("发送(S)", ControlType.Button);
+            var btn_SendMessage = GetWeChatControl(wechatWindow,"发送(S)", ControlType.Button);
+            if (btn_SendMessage == null)
+            {
+                btn_SendMessage = GetWeChatControl(wechatWindow, "sendBtn", ControlType.Button);
+            }
             if (btn_SendMessage == null)
             {
                 WriteLog("没有找到「发送消息」的按钮。");
@@ -118,20 +146,13 @@ namespace WeChatAutomationDemo
             }
             rect = btn_SendMessage.BoundingRectangle;
             //这里如果是中文输入法的话，输入英文可能出现问题，这里暂时使用粘贴方式解决
-            //edit_MessageInput.AsTextBox().Enter(messageToSend);
-            for (int i = 0; i < 10; i++)
-            {
-                messageToSend = string.Format("{0}   {1}    {2}",i+1, DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss.FFF"), "UI Automation for WeChat(By:WuTao).");
-                Clipboard.SetText(messageToSend);
-                Keyboard.Pressing(VirtualKeyShort.CONTROL);
-                Keyboard.Press(VirtualKeyShort.KEY_V);
-                Keyboard.Release(VirtualKeyShort.CONTROL);
-                Mouse.Click(new Point(rect.X + (rect.Width / 2), rect.Y + (rect.Height / 2)));
-                Thread.Sleep(500);
-            }
-
-            WriteLog("消息发送成功，关闭微信窗口");
-            Wnd_WeChat.Close();
+            Clipboard.SetText(message);
+            Keyboard.Pressing(VirtualKeyShort.CONTROL);
+            Keyboard.Press(VirtualKeyShort.KEY_V);
+            Keyboard.Release(VirtualKeyShort.CONTROL);
+            Mouse.Click(new Point(rect.X + (rect.Width / 2), rect.Y + (rect.Height / 2)));
+            WriteLog("发送成功。");
+            Thread.Sleep(500);
         }
 
         private int GetProcessId(string processName)
@@ -146,7 +167,7 @@ namespace WeChatAutomationDemo
 
         private void WriteLog(string log)
         {
-            textBox1.AppendText(string.Format("{0}\t{1}{2}", DateTime.Now.ToString("HH:mm:ss"), log, Environment.NewLine));
+            TextBox_Log.AppendText(string.Format("{0}  {1}{2}", DateTime.Now.ToString("HH:mm:ss"), log, Environment.NewLine));
         }
 
         /// <summary>
@@ -156,32 +177,13 @@ namespace WeChatAutomationDemo
         /// <returns></returns>
         private bool GetWindowVisibleStatus(string windowName)
         {
-            if(Desktop == null)
+            var desktop = new UIA3Automation().GetDesktop();
+            if (desktop == null)
             {
-                Desktop = new UIA3Automation().GetDesktop();
+                desktop = new UIA3Automation().GetDesktop();
             }
-            var element = Desktop.FindFirstDescendant(cf => cf.ByName(windowName));
+            var element = desktop.FindFirstDescendant(cf => cf.ByName(windowName));
             return !(element == null);
-        }
-
-        private void Btn_WeChatButtonTest_Click(object sender, EventArgs e)
-        {
-            if(Wnd_WeChat == null)
-            {
-                WriteLog("请先激活微信。");
-                return;
-            }
-            Wnd_WeChat.Focus();
-            var elements = Wnd_WeChat.FindAllDescendants();
-            foreach (var element in elements)
-            {
-                if(element.ControlType == FlaUI.Core.Definitions.ControlType.Button)
-                {
-                    //element.DrawHighlight();
-                    WriteLog("微信按钮：" + element.Name);
-                    Thread.Sleep(100);
-                }
-            }
         }
 
         /// <summary>
@@ -189,14 +191,14 @@ namespace WeChatAutomationDemo
         /// </summary>
         /// <param name="buttonName"></param>
         /// <returns></returns>
-        private AutomationElement GetWeChatControl(string controlName, ControlType controlType)
+        private AutomationElement GetWeChatControl(in Window wechatWindow, string controlName, ControlType controlType)
         {
-            if (Wnd_WeChat == null)
+            if (wechatWindow == null)
             {
                 return null;
             }
-            Wnd_WeChat.Focus();
-            var elements = Wnd_WeChat.FindAllDescendants();
+            wechatWindow.Focus();
+            var elements = wechatWindow.FindAllDescendants();
             foreach (var element in elements)
             {
                 if (element.Name == controlName && element.ControlType == controlType)
@@ -206,6 +208,97 @@ namespace WeChatAutomationDemo
 
             }
             return null;
+        }
+
+        private void Btn_FindFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "微信主程序(wechat.exe)|*.exe";
+            if(openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                TextBox_AppPath.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if(File.Exists(appDefaultPath))
+            {
+                TextBox_AppPath.Text = appDefaultPath;
+            }
+            dateTimePicker1.CustomFormat = "yyyy-MM-dd HH:mm:ss";
+            dateTimePicker1.Enabled = CheckBox_ScheduleTask.Checked;
+        }
+
+        private void CheckBox_ScheduleTask_CheckedChanged(object sender, EventArgs e)
+        {
+            dateTimePicker1.Enabled = CheckBox_ScheduleTask.Checked;
+            if(CheckBox_ScheduleTask.Checked)
+            {
+                Button_SendMessage.Text = "开始任务";
+            }
+            else
+            {
+                Button_SendMessage.Text = "发送消息";
+            }
+        }
+
+        private void StartProgress()
+        {
+            timer1.Enabled = false;
+            Button_SendMessage.Text = "发送";
+            var app = GetWeChatApplication();
+            if (app == null)
+            {
+                WriteLog("没有获取到微信的主程序。");
+                return;
+            }
+            var wechatMainWindow = GetWeChatMainWindow(ref app);
+            wechatMainWindow.Focus();
+            if (wechatMainWindow == null)
+            {
+                WriteLog("没有获取到微信的主窗口");
+                return;
+            }
+            var chatBox = SearchTarget(wechatMainWindow, TextBox_SendTarget.Text);
+            if (chatBox == null)
+            {
+                WriteLog("没有获取到消息的输入框");
+                return;
+            }
+            SendMessage(wechatMainWindow, chatBox, TextBox_Message.Text);
+        }
+
+        private void Button_SendMessage_Click(object sender, EventArgs e)
+        {
+            if(timer1.Enabled)
+            {
+                timer1.Enabled = false;
+                WriteLog("停止计划任务。");
+                Button_SendMessage.Text = "发送";
+            }
+            if (CheckBox_ScheduleTask.Checked)
+            {
+                timer1.Enabled = true;
+                WriteLog("开始执行定时任务。");
+                Button_SendMessage.Text = "执行中";
+            }
+            else
+            {
+                StartProgress();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(dateTimePicker1.Value.Date == DateTime.Now.Date &&
+                dateTimePicker1.Value.Hour == DateTime.Now.Hour &&
+                dateTimePicker1.Value.Minute == DateTime.Now.Minute &&
+                dateTimePicker1.Value.Second == DateTime.Now.Second)
+            {
+                WriteLog($"到达指定时间：{dateTimePicker1.Value}，开始执行定时任务。");
+                StartProgress();
+            }
         }
     }
 }
